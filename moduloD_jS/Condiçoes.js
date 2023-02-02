b6819abfe458da165add3ab2d31cfcbbdce61773
Manoel Roberto Korpalski <manoelroberto.dev@gmail.com>
2023-02-02 17:14:43 -0300

    condicional

---
+++ moduloD_jS/Condiçoes.js
@@ -0,0 +1,10 @@
+var vel = 8
+console.log(`A velocidade do seu carro é ${vel}km/h.`)
+
+if (vel > 80){
+    console.log(`Você ultrapassou o limite de velocidade.Multado!`)
+}
+
+else {
+    console.log(`Boa Viagem!`)
+}
