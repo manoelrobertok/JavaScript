232ac0119b5adaccf9312509b03871cb0f5c01d4
Manoel Roberto Korpalski <manoelroberto.dev@gmail.com>
2023-02-28 20:21:43 -0300

    atualizado

--- Exercicio_Idade/Contando_numeros/contando.js
+++ Exercicio_Idade/Contando_numeros/contando.js
@@ -8,16 +8,31 @@ function contar(){
   /*   teste ok!!  */
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){ window.alert('[ERRO] Faltam dados!')
     }  
+
      else{
-        res.innerHTML = 'Contando...'
+        res.innerHTML = 'Contando... <br>'
+
         let i = Number(ini.value) // convertendo o valor em número.
         let f = Number(fim.value)
         let p = Number(passo.value)
 
-        for(c = i; c <= f; c += p){ // C é o contador
-            res.innerHTML += `${c} \u{1F449} ` // Buscamos o imoji no site, unicode.or e o formatamos para JavaScript.
-             }
-         }
+        if (i < f){ //Contagem crescente.
+        for(let c = i; c <= f; c += p){ 
+          
+          // O contador vai começar no início, e enquanto o contador for menor ou igual ao fim, o contador vai receber ele mesmo, mais o passo!
+
+            res.innerHTML += `${c} \u{1F449} ` // Buscamos o imoji no site, unicode.org e o formatamos para JavaScript( \u{} ).
+        }
+      }
+      else{
+          for(let c = i; c >= f; c -= p){
+            innerHTML += `${c} \u{1F449}`
+          }
+        }
+
+        innerHTML += `\u{1F3C1}`
+     }
+             
+   }
         
 
-} 
