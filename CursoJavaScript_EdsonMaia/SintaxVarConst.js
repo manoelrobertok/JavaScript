6cbb76634099dcb7a607859cc93aab0f56aa972a
Manoel Roberto Korpalski <manoelroberto.dev@gmail.com>
2023-03-06 15:58:44 -0300

    atualização

---
+++ CursoJavaScript_EdsonMaia/SintaxVarConst.js
@@ -0,0 +1,23 @@
+/*Criar variáveis e atribuir valores*/
+    let mensagem = 'Eu sou programador Front-End.'
+    let mensagem2 = 'Quero ser um excelente programador.'
+    let nome = 'Manoel Roberto Korpalski'
+    let idade = 39
+    let alt = 1.74
+    let peso = 84
+
+/* Criar constantes e atribuir valores*/
+    const PI = 3.1415
+    const TAXA = 0.5
+
+/* Escrever Variáveis e Cosntantes na Tela*/
+
+    document.write(mensagem)
+    document.write(mensagem2)
+    document.write(nome)
+    document.write(idade)
+    document.write(alt)
+    document.write(peso)
+
+
+
---
+++ OperadoresOperacao/operadoes.js
@@ -0,0 +1,30 @@
+/*Operadores e operações*/  
+
+let n1 = 10 //Number (inteiro)
+let n2 = 2 // Number (inteiro)
+let n3 = "2" // string (texto)
+
+n1 + n2 //adição
+n1 - n2 //subtração
+n1 * n2 //multiplicação
+n1 / n2 //divisão
+n1 % n2 //resto da dívisão ou módulo
+
+n2 == n3 // n2 igual a n3
+n2 === n3 //n2 idêntico a n3
+n2 != n3 //diferente de 
+n2 !== n3 // não é idêntico
+
+let a = true
+let b = false
+
+a && b // a e b
+a || b // a ou b 
+!a // não
+!b // não
+
+n1 < n2 // menor que
+n1 > n2 // maior que
+n1 <= n2 //menor ou igual
+n1 >= n2 // maior ou igual
+
